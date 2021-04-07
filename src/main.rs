deb00c4fefddb975f8822c66a3e5c68406e6f0c9
vapvin <tpehdj12@daum.net>
2021-04-07 14:08:51 +0900

    Rust Same Range loop

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
-use rand::Rng;
+use rand::distributions::{Distribution, Uniform};
 
 fn main() {
     let mut rng = rand::thread_rng();
-    println!("Integer: {}", rng.gen_range(0..10));
-    println!("Float: {}", rng.gen_range(0.0..10.0));
+    let die = Uniform::from(1..7);
+
+    loop {
+        let throw = die.sample(&mut rng);
+        println!("Roll the die: {}", throw);
+        if throw == 6 {
+            break;
+        }
+    }
 }
