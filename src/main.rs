e9588a34e0353f2e54708eae271c898950b64e76
vapvin <tpehdj12@daum.net>
2021-04-07 14:07:14 +0900

    Rust Random number with range

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,6 @@ use rand::Rng;
 
 fn main() {
     let mut rng = rand::thread_rng();
-
-    let n1: u8 = rng.gen();
-    let n2: u16 = rng.gen();
-    println!("Random U8: {}", n1);
-    println!("Random U16: {}", n2);
-    println!("Random U32: {}", rng.gen::<u32>());
+    println!("Integer: {}", rng.gen_range(0..10));
+    println!("Float: {}", rng.gen_range(0.0..10.0));
 }
