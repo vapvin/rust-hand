688503daebc80ad8a256e739b3f51fef3ef55cd0
vapvin <tpehdj12@daum.net>
2021-04-07 14:17:23 +0900

    Rnadom Generate with Custom type

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
-use rand_distr::{Distribution, Normal, NormalError};
-use rand::thread_rng;
+use rand::Rng;
+use rand::distributions::{Distribution, Standard};
 
-fn main() -> Result<(), NormalError> {
-    let mut rng = thread_rng();
-    let normal = Normal::new(2.0, 3.0)?;
-    let v = normal.sample(&mut rng);
-    println!("{} is from a N(2, 9) distribution", v);
-    Ok(())
+#[derive(Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Distribution<Point> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Point {
+        let (rand_x, rand_y) = rng.gen();
+        Point {
+            x: rand_x,
+            y: rand_y,
+        }
+    }
+}
+
+fn main() {
+    let mut rng = rand::thread_rng();
+    let rand_tuple = rng.gen::<(i32, bool, f64)>();
+    let rand_point: Point = rng.gen();
+    println!("Random tuple: {:?}", rand_tuple);
+    println!("Random Point: {:?}", rand_point);
 }
