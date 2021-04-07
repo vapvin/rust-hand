b9d4f99d6b2b2214a2ce53187a30fda48f1fd409
vapvin <tpehdj12@daum.net>
2021-04-07 14:23:47 +0900

    Create Random password from a set of user-defined

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
-use rand::{thread_rng, Rng};
-use rand::distributions::Alphanumeric;
-
 fn main() {
-    let rand_string: String = thread_rng()
-        .sample_iter(&Alphanumeric)
-        .take(30)
-        .map(char::from)
+    use rand::Rng;
+    const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+                            abcdefghijklmnopqrstuvwxyz\
+                            0123456789)(*&^%$#@!~";
+    const PASSWORD_LEN: usize = 30;
+    let mut rng = rand::thread_rng();
+
+    let password: String = (0..PASSWORD_LEN)
+        .map(|_| {
+            let idx = rng.gen_range(0..CHARSET.len());
+            CHARSET[idx] as char
+        })
         .collect();
-    println!("{}", rand_string);
+
+    println!("{:?}", password);
 }
