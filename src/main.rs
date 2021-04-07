41739994cecbba5b4cf8a07bf12644cd7e924cde
vapvin <tpehdj12@daum.net>
2021-04-07 14:06:25 +0900

    Rust Random number with Type

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::thread_rng();
+
+    let n1: u8 = rng.gen();
+    let n2: u16 = rng.gen();
+    println!("Random U8: {}", n1);
+    println!("Random U16: {}", n2);
+    println!("Random U32: {}", rng.gen::<u32>());
 }
